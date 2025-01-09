b87f79e5f19956b65e26a83be0c8312dc73d1477
Ashwin <ashvinkumar8891@gmail.com>
2025-01-09 23:05:29 +0530

    functionality

--- calc/src/components/Calculator.jsx
+++ calc/src/components/Calculator.jsx
@@ -3,18 +3,24 @@ import React, { useState } from "react";
 const Calculator = () => {
   const [result, setResult] = useState(null);
   const [display, setDisplay] = useState("");
-  let arr = [];
 
   function displayCharacters(btn) {
-    // arr.push(btn);
     if (btn !== "=" && btn !== "C") {
       setDisplay((prev) => prev + btn);
-      console.log(btn);
     }
     if (btn === "C") {
       setDisplay("");
+      setResult(null);
+    }
+    if (btn === "=" && display !== "") {
+      setResult(eval(display));
+    }
+    if (btn === "=" && display === "") {
+      setResult("Error");
     }
   }
+
+  console.log("result::", result);
 
   let buttons = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"];
   let keypad = buttons.map((btn) => {
@@ -40,14 +46,17 @@ const Calculator = () => {
   return (
     <div>
       <h1>React Calculator</h1>
-      <div style={{ marginBottom: "50px" }}>
+      <div>
         <input
           type="text"
-          style={{ height: "30px", width: "300px" }}
+          style={{ height: "30px", width: "300px", fontSize: "20px" }}
           value={display}
+          readOnly={true}
         />
       </div>
-      <div>{result}</div>
+      <div style={{ textAlign: "center" }}>
+        <p style={{ fontSize: "24px", color: "grey" }}>{result}</p>
+      </div>
       <div
         style={{
           display: "grid",
